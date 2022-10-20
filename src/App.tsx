543f3bcdbcaa58f9d44f9722caec1c65a7e03fc4
Louis2602 <trantunglam135790@gmail.com>
2022-10-20 14:28:17 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Pokemon, Detail } from './interface';
+import SearchBar from './components/SearchBar';
+import PokemonCollections from './components/PokemonCollections';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Pokemons {
+	name: string;
+	url: string;
 }
+
+const App: React.FC = () => {
+	const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+	const [nextUrl, setNextUrl] = useState<string>('');
+	const [loading, setLoading] = useState<boolean>(true);
+	const [viewDetail, setDetail] = useState<Detail>({
+		id: 0,
+		isOpened: false,
+	});
+	const [searchTerm, setSearchTerm] = useState<string>('');
+
+	useEffect(() => {
+		const getPokemon = async () => {
+			const res = await axios.get(
+				'https://pokeapi.co/api/v2/pokemon?limit=20&offset=20'
+			);
+			setNextUrl(res.data.next);
+			res.data.results.forEach(async (pokemon: Pokemons) => {
+				const poke = await axios.get(
+					`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+				);
+				setPokemons((p) => [...p, poke.data]);
+				setLoading(false);
+			});
+		};
+		getPokemon();
+	}, []);
+
+	const handleLoadMore = async () => {
+		setLoading(true);
+		let res = await axios.get(nextUrl);
+		setNextUrl(res.data.next);
+		res.data.results.forEach(async (pokemon: Pokemons) => {
+			const poke = await axios.get(
+				`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
+			);
+			setPokemons((p) => [...p, poke.data]);
+			setLoading(false);
+		});
+	};
+
+	return (
+		<div className='App'>
+			<div className='container'>
+				<header className='pokemon-header'>Pokemon</header>
+				<SearchBar
+					pokemons={pokemons}
+					viewDetail={viewDetail}
+					setDetail={setDetail}
+					searchTerm={searchTerm}
+					setSearchTerm={setSearchTerm}
+				/>
+				{!searchTerm && (
+					<PokemonCollections
+						pokemons={pokemons}
+						viewDetail={viewDetail}
+						setDetail={setDetail}
+					/>
+				)}
+				{!viewDetail.isOpened && (
+					<div className='btn'>
+						<button onClick={handleLoadMore}>
+							{loading ? 'Loading...' : 'Load more'}
+						</button>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/PokemonCollections.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PokemonList from './PokemonList';
+import './pokemon.css';
+import { PokemonDetail, Detail } from '../interface';
+
+export interface Props {
+	pokemons: PokemonDetail[];
+	viewDetail: Detail;
+	setDetail: React.Dispatch<React.SetStateAction<Detail>>;
+}
+
+const PokemonCollections: React.FC<Props> = ({
+	pokemons,
+	viewDetail,
+	setDetail,
+}) => {
+	const selectPokemon = (id: number) => {
+		if (!viewDetail.isOpened) {
+			setDetail({
+				id: id,
+				isOpened: true,
+			});
+		}
+	};
+	return (
+		<>
+			<section
+				className={
+					viewDetail.isOpened
+						? 'collection-container-active'
+						: 'collection-container'
+				}
+			>
+				{viewDetail.isOpened ? (
+					<div className='overlay'></div>
+				) : (
+					<div></div>
+				)}
+				{pokemons.map((pokemon, idx) => {
+					return (
+						<div
+							onClick={() => {
+								selectPokemon(pokemon.id);
+								console.log(pokemon.id);
+							}}
+							key={idx}
+						>
+							<PokemonList
+								viewDetail={viewDetail}
+								setDetail={setDetail}
+								name={pokemon.name}
+								id={pokemon.id}
+								abilities={pokemon.abilities}
+								image={pokemon.sprites.front_default}
+							/>
+						</div>
+					);
+				})}
+			</section>
+		</>
+	);
+};
+
+export default PokemonCollections;
